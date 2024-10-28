f9109b680712ab22f623f1fa6bce3430005b98a1
drewzambelli <drewzambelli@gmail.com>
2024-10-28 10:12:36 -0400

    first push - credits/debits now appear on localhost

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,6 +5,7 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import React, {Component} from 'react';
 
 const Credits = (props) => {
   return (

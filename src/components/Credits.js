d41e0f5807c73d2bba01b9a3ea11e6b95a91b526
drewzambelli <drewzambelli@gmail.com>
2024-10-29 12:18:06 -0400

    added some additional styling to money ($ etc)

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -17,7 +17,7 @@ const Credits = (props) => {
     const { credits } = props;
     return credits.map((credit) => {
       let date = credit.date.slice(0, 10);
-      return <li key={credit.id}>${credit.amount} {credit.description} {date}</li>
+      return <li key={credit.id}> ${credit.amount} ** <strong>{credit.description}</strong> ** {date}</li>
     });
   };
 
